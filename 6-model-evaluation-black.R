58e1715a6a4a105bd738b950fd908731c2cdffed
aeclarke <aeclarke@wharton.upenn.edu>
2021-12-15 15:29:40 -0500

    RMSE summary

--- 6-model-evaluation-black.R
+++ 6-model-evaluation-black.R
@@ -7,53 +7,53 @@ setwd("~/Desktop/STAT471/unemployment-project")
 unemploy_test = read_tsv("data/clean/econ_test.tsv")
 
 #load least squares regression
-load("results/lm_fit.Rda")
+load("results/lm_fit_black.Rda")
 
 # load ridge fit object
-load("results/ridge_fit.Rda")
+load("results/ridge_fit_black.Rda")
 
 # load lasso fit object
-load("results/lasso_fit.Rda")
+load("results/lasso_fit_black.Rda")
 
 # load tree fit object
-load("results/tree_fit.Rda")
+load("results/tree_fit_black.Rda")
 
 # load pruned tree fit object
-load("results/optimal_tree.Rda")
+load("results/optimal_tree_black.Rda")
 
 # load random forest
-load("results/rf_fit.Rda")
+load("results/rf_fit_black.Rda")
 
 # generate linear model predictions and test error
-linear_predictions = predict(lm_fit, newdata = unemploy_test)
-linear_RMSE = sqrt(mean((linear_predictions - unemploy_test$unemployment_rate)^2))
+linear_predictions = predict(lm_fit_black, newdata = unemploy_test)
+linear_RMSE = sqrt(mean((linear_predictions - unemploy_test$black_unemployment)^2))
 
 # generate ridge regression model predictions and test error
-ridge_predictions = predict(ridge_fit, 
+ridge_predictions = predict(ridge_fit_black, 
                             newdata = unemploy_test, 
                             s = "lambda.1se")
-ridge_RMSE = sqrt(mean((ridge_predictions - unemploy_test$unemployment_rate)^2))
+ridge_RMSE = sqrt(mean((ridge_predictions - unemploy_test$black_unemployment)^2))
 
 # generate lasso regression model predictions and test error
-lasso_predictions = predict(lasso_fit, 
+lasso_predictions = predict(lasso_fit_black, 
                             newdata = unemploy_test,
                             s = "lambda.1se")
-lasso_RMSE = sqrt(mean((lasso_predictions -  unemploy_test$unemployment_rate)^2))
+lasso_RMSE = sqrt(mean((lasso_predictions -  unemploy_test$black_unemployment)^2))
 
 # generate decision tree predictions and test error
-tree_predictions = predict(tree_fit, 
+tree_predictions = predict(tree_fit_black, 
                            newdata = unemploy_test)
-tree_RMSE = sqrt(mean((tree_predictions -  unemploy_test$unemployment_rate)^2))
+tree_RMSE = sqrt(mean((tree_predictions -  unemploy_test$black_unemployment)^2))
 
 # generate pruned decision tree predictions and test error
-pruned_tree_predictions = predict(optimal_tree, 
+pruned_tree_predictions = predict(optimal_tree_black, 
                                   newdata = unemploy_test)
-pruned_tree_RMSE = sqrt(mean((pruned_tree_predictions -  unemploy_test$unemployment_rate)^2))
+pruned_tree_RMSE = sqrt(mean((pruned_tree_predictions -  unemploy_test$black_unemployment)^2))
 
 # generate random forest predictions and test error
-rf_predictions = predict(rf_fit, 
+rf_predictions = predict(rf_fit_black, 
                          newdata = unemploy_test)
-rf_RMSE = sqrt(mean((rf_predictions -  unemploy_test$unemployment_rate)^2))
+rf_RMSE = sqrt(mean((rf_predictions -  unemploy_test$black_unemployment)^2))
 
 # create table of these three model test errors
 error_for_models = tribble(
@@ -61,7 +61,7 @@ error_for_models = tribble(
   #------/------- 
   "Linear", linear_RMSE,
   "Ridge", ridge_RMSE,
-  "Lasso", lasso_RMSE,
+  "Lasso", lasso_RMSE, 
   "Random Forest", rf_RMSE
 )
 
@@ -74,5 +74,5 @@ error_for_models %>% kable(format = "latex", row.names = NA,
   kable_styling(position = "center") %>%
   kable_styling(latex_options = "HOLD_position")  %>%
   save_kable(file =
-               "~/Desktop/STAT471/unemployment-project/results/RMSE-models.pdf", 
+               "~/Desktop/STAT471/unemployment-project/results/RMSE-models_black.pdf", 
              self_contained = T)
